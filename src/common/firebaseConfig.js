0823ab8174e7a721cf2799bcb1f737115eab5a28
Ilia Koliesnikov <bukovskii@ua.fm>
2022-06-01 13:24:17 +0300

    first commit

---
+++ src/common/firebaseConfig.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-mutable-exports, @typescript-eslint/ban-types
+const firebaseConfig = {
+    apiKey: "AIzaSyD0LcDMSiFDZQ9LVAtMPRcm5ce45bAHv8w",
+    authDomain: "test-fire-71eb1.firebaseapp.com",
+    projectId: "test-fire-71eb1",
+    storageBucket: "test-fire-71eb1.appspot.com",
+    messagingSenderId: "328872157156",
+    appId: "1:328872157156:web:9e8638b833860235f2b1f1"
+};
+
+export default firebaseConfig;
